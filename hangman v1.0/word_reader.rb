daf0f2d42f33617c90eef7504108ec6866a3a2a3
R4smuS-jpg <d.gaysin@mail.ru>
2022-04-22 16:26:20 +0300

    some minor fixes + solve bug with empty word

--- hangman v1.0/word_reader.rb
+++ hangman v1.0/word_reader.rb
@@ -1,5 +1,4 @@
 # I think each method from this class is pretty clear
-
 class WordReader
   def read_from_argv
     return ARGV[0]
@@ -10,23 +9,28 @@ class WordReader
   end
 
   def read_from_user
-    puts "0 - enter a word by youself\n1 - let program choose you a word"
-    choose = STDIN.gets.chomp.to_i
+    puts "0 - enter a word by yourself\n1 - let the program choose you a word"
+    choose = STDIN.gets.chomp
 
-    if choose == 0
+    while !choose.match(/^[0-1]$/)
+      print "Incorrect input. Try again: "
+      choose = STDIN.gets.chomp
+    end
+
+    if choose == "0"
+      system "clear"
+
       print "Enter word: "
       word = STDIN.gets.encode("UTF-8").chomp.downcase
 
-      while !word.match(/[a-z]/)
+      while !word.match(/[a-z]/) || word.include?(" ") 
         print "Incorrect word. Try again: "
         word = STDIN.gets.encode("UTF-8").chomp.downcase
       end
 
       return word
-    elsif choose == 1 
-      return read_from_file
     else
-      puts "Incorrect input."
+      return read_from_file
     end
   end
 end
