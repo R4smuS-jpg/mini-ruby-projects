6b6c9d5f8729fecf169378ff5c03e8fcf8fc493d
R4smuS-jpg <d.gaysin@mail.ru>
2022-05-19 22:36:00 +0300

    edit readme + small fix

--- diary/reader.rb
+++ diary/reader.rb
@@ -1,18 +1,18 @@
 class Reader
   def owner_present?
-    diary_file = File.new("./diary.txt", "r:UTF-8")
+    diary_file = File.new("./data/diary.txt", "r:UTF-8")
 
     if diary_file.read == ""
       return false
+      diary_file.close
     else
       return true
+      diary_file.close
     end
-
-    diary_file.close
   end
 
   def read_all
-    diary_file = File.new("./diary.txt", "r:UTF-8")
+    diary_file = File.new("./data/diary.txt", "r:UTF-8")
 
     content = diary_file.read 
     diary_file.close
