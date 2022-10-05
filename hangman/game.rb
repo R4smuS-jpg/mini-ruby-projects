fd18b04559e686ac3ccf15a79b63935899fa3ba3
R4smuS-jpg <r4smusjpg@gmail.com>
2022-10-05 21:32:21 +0300

    put some methods from Game class to private zone

--- hangman/game.rb
+++ hangman/game.rb
@@ -57,48 +57,50 @@ class Game
     end
   end
 
-  # Returns new user
-  def create_user
-    print 'Enter your name: '
-    username = $stdin.gets.encode('UTF-8').chomp.strip
-    User.new(username)
-  end
+  private
+  
+    # Returns new user
+    def create_user
+      print 'Enter your name: '
+      username = $stdin.gets.encode('UTF-8').chomp.strip
+      User.new(username)
+    end
 
-  # Save score to file
-  def save_user_score
-    scoreboard_path = './data/scoreboard.txt'
-    file = File.open(scoreboard_path, 'r:UTF-8')
-    scores = file.readlines
-    file.close
-    scores.map! {|s| s.chomp}
+    # Save score to file
+    def save_user_score
+      scoreboard_path = './data/scoreboard.txt'
+      file = File.open(scoreboard_path, 'r:UTF-8')
+      scores = file.readlines
+      file.close
+      scores.map! {|s| s.chomp}
 
-    user_has_record = false
-    scores.each do |s|
-      if s.include?(@user.name)
-        user_has_record = true
-        scores.map! { |s| s.include?(@user.name) && @user.time < s.split[2].to_f ? s = @user.to_score_record : s }
-        break
+      user_has_record = false
+      scores.each do |s|
+        if s.include?(@user.name)
+          user_has_record = true
+          scores.map! { |s| s.include?(@user.name) && @user.time < s.split[2].to_f ? s = @user.to_score_record : s }
+          break
+        end
       end
+
+      scores.push(@user.to_score_record) unless user_has_record
+      scores.sort_by! { |s| s.split[2].to_f }
+
+      file = File.open(scoreboard_path, 'w:UTF-8')
+      file.write(scores.join("\n"))
+      file.close
     end
 
-    scores.push(@user.to_score_record) unless user_has_record
-    scores.sort_by! { |s| s.split[2].to_f }
+    # Method that requests letter
+    def ask_letter
+      print "\nGuess one letter: "
+      letter = $stdin.gets.encode('UTF-8').chomp.downcase.strip
 
-    file = File.open(scoreboard_path, 'w:UTF-8')
-    file.write(scores.join("\n"))
-    file.close
-  end
+      until letter.match(/^[a-z]$/)
+        print 'Incorrect letter. Try again: '
+        letter = $stdin.gets.encode('UTF-8').chomp.downcase
+      end
 
-  # Method that requests letter
-  def ask_letter
-    print "\nGuess one letter: "
-    letter = $stdin.gets.encode('UTF-8').chomp.downcase.strip
-
-    until letter.match(/^[a-z]$/)
-      print 'Incorrect letter. Try again: '
-      letter = $stdin.gets.encode('UTF-8').chomp.downcase
+      letter
     end
-
-    letter
-  end
 end
